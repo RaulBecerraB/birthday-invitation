7e9cd02f0fdb54a79f9d2bfae583236e70327afb
DigitalBecerra21 <ingenierobecerrabarcelo@gmail.com>
2025-02-17 08:34:06 -0600

    feat(NavBar): Add selected page button animation

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,9 +1,12 @@
 "use client";
 import React from "react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import links from "./navlinks.json";
 
 export default function NavBar() {
+  const pathname = usePathname();
+
   return (
     <div className="w-full max-w-4xl mx-auto mt-4 mb-8">
       <div className="bg-white shadow-md shadow-black/20 rounded-[20px] p-2">
@@ -11,7 +14,13 @@ export default function NavBar() {
           <div className="flex justify-center space-x-12">
             {links.map((link, index) => (
               <Link key={index} href={link.href}>
-                <h2 className="text-[#1e3a8a] text-lg font-semibold hover:opacity-80 transition">
+                <h2
+                  className={`text-lg font-semibold transition px-3 py-1 rounded-md
+                    ${pathname === link.href
+                      ? 'bg-primary text-white'
+                      : 'text-[#1e3a8a] hover:opacity-80'
+                    }`}
+                >
                   {link.name}
                 </h2>
               </Link>
